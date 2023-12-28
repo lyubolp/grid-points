9940e846c5a64e82840476fabc68b0a69267e728
Lyuboslav Karev <lyubo.karev@gmail.com>
2023-12-28 14:46:11 +0200

    Added docstrings and fixed __eq__ typing

--- src/point.py
+++ src/point.py
@@ -20,50 +20,116 @@ class Point(ABC):
     def __iter__(self) -> GridIterator:
         """
         Returns an GridIterator, which can be used for iteration over the grid
+        :rtype: GridIterator
         """
-        pass
-    
+
     @abstractmethod
     def __str__(self) -> str:
-        # TODO - Docstring
-        pass
+        """
+        String representation of the Point
+        :rtype: str
+        """
 
     @abstractmethod
     def __repr__(self) -> str:
-        # TODO - Docstring
-        pass
+        """Python representation of the Point
+        :rtype: str
+        """
 
     @abstractmethod
     def __hash__(self) -> int:
-        # TODO - Docstring
         pass
-    
+
     @abstractmethod
-    def __eq__(self, other: "Point") -> bool:
-        pass
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Point):
+            # https://mypy.readthedocs.io/en/stable/common_issues.html#incompatible-overrides
+            raise ValueError("Invalid type for other")
+
+        return True
 
     @abstractmethod
     def __add__(self, other: "Point") -> "Point":
-        pass
+        """
+        Add two points together.
+        For example - `Point(x1, y1) + Point(x2, y2) = Point(x1 + x2, y1 + y2)`
+
+        :param other: The point that will be added
+        :type other: Point
+        :return: Resulting point
+        :rtype: Point
+        """
 
     @abstractmethod
     def __sub__(self, other: "Point") -> "Point":
-        pass
+        """
+        Subtract a point from the current one
+        For example - `Point(x1, y1) - Point(x2, y2) = Point(x1 - x2, y1 - y2)`
+
+        :param other: The point that will be subtracted
+        :type other: Point
+        :return: Resulting point
+        :rtype: Point
+        """
 
     @abstractmethod
     def __mul__(self, other: "Point") -> "Point":
-        pass
+        """
+        Multiply two points together.
+        For example - `Point(x1, y1) * Point(x2, y2) = Point(x1 * x2, y1 * y2)`
+
+        :param other: The point that will be added
+        :type other: Point
+        :return: Resulting point
+        :rtype: Point
+        """
 
     @abstractmethod
     def __floordiv__(self, other: "Point") -> "Point":
-        pass
-    
+        """
+        Divide a point from the current one
+        For example - `Point(x1, y1) // Point(x2, y2) = Point(x1 // x2, y1 // y2)`
+
+        :param other: The divisor point
+        :type other: Point
+        :return: Resulting point
+        :rtype: Point
+        """
+
     @abstractmethod
     def distance_to(self, other: "Point") -> float:
-        # TODO - Docstring
-        pass
-    
+        """
+        Calculate the distance between the current point an any other one
+
+        :param other: The Point to calculate the distance to
+        :type other: Point
+        :return: The Eucledian distance between the points
+        :rtype: float
+        """
+
     @abstractmethod
     def is_within(self, end: "Point", start: Optional["Point"] = None) -> bool:
-        # TODO - Docstring
-        pass
+        """
+        Check if a point is inside bounds. Upper bounds are exclusive.
+        Default lower bounds are 0
+
+        2D example:
+        ```python
+        bounds = Point2D(3, 4)
+        a = Point2D(2, 3)
+        b = Point2D(3, 4)
+        c = Point2D(-1, -2)
+
+        a.is_within(bounds)  # True
+        b.is_within(bounds)  # False
+        c.is_within(bounds)  # False
+        ```
+
+        :param end: The upper bounds of the region
+        :type end: Point
+        :param start: The lower bounds of the region, defaults to None,
+          which is interpreted to the 0-point in the space ((0, 0) for 2D, (0, 0, 0) for 3D)
+        :type start: Optional[Point], optional
+        :return: If the current point is inside the specified bounds
+        :rtype: bool
+        """
