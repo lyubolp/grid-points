f7725332b1f7a7a10ec39df6f2025dc4a415ca26
Lyuboslav Karev <lyubo.karev@gmail.com>
2023-12-28 15:17:35 +0200

    Added init to grid_iterator

--- src/grid_iterator/grid_iterator.py
+++ src/grid_iterator/grid_iterator.py
@@ -1,8 +1,23 @@
+"""
+Module containing the abstract class for grid iteration
+"""
 from abc import ABC, abstractmethod
+from typing import Optional
 
 from src.point import Point
 
 
 class GridIterator(ABC):
+    """
+    Represents an abstract grid iterator over a Nth dimentional grid
+
+    :param ABC: _description_
+    :type ABC: _type_
+    """
+    def __init__(self, end: Point, start: Optional[Point] = None):
+        self._end = end
+        self._start = start
+
+    @abstractmethod
     def __next__(self) -> Point:
         pass
